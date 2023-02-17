ff2ed00c7fa98f63d39926effc7af1deb3e6774a
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-17 23:21:33 +0100

    reducer

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,27 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useReducer } from "react";
 import data from "./data";
 
 const AppContext = React.createContext();
 
+const defaultState = {
+  items: data,
+}
+
+const CLEAR_CART = 'CLEAR_CART'
+
 const AppProvider = ({ children }) => {
   const [itemData, setItemData] = useState(data);
   const [itemNumber, setItemNumber] = useState(1);
+
+  const reducer = (state, action)=>{
+    if(action.type === CLEAR_CART){
+      return {...state, item: []}
+    }
+
+    throw new Error('error occured')
+  }
+
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
   function add(id) {
     const item = itemData.find(item => item.id === id)
@@ -23,7 +39,8 @@ const AppProvider = ({ children }) => {
   }
 
   function clearCart(){
-    setItemData([])
+    dispatch({type: CLEAR_CART})
+    // setItemData([])
   }
 
   return (
